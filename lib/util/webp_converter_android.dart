0c77cf8c64cf853aaf6a5a40ed1af7f521fbf630
n7484443 <n7484443@naver.com>
2022-03-05 19:01:05 +0900

    -안드로이드에서 좀 더 가벼운 라이브러리로 수정

--- lib/util/webp_converter_android.dart
+++ lib/util/webp_converter_android.dart
@@ -1,17 +1,16 @@
 import 'dart:typed_data';
 import 'package:cyoap_flutter/util/webp_converter.dart';
-import 'package:image_compression_flutter/image_compression_flutter.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 
 class WebpConverterImp extends WebpConverter{
-  Configuration config = const Configuration(
-    outputType: ImageOutputType.webpThenPng,
-    quality: 100,
-  );
   Future<Uint8List> convert(Uint8List input, String type) async {
-    ImageFile inputImage = ImageFile(rawBytes: input, filePath: 'exported.$type');
-    final param = ImageFileConfiguration(input: inputImage, config: config);
-    final output = await compressor.compress(param);
-    return output.rawBytes;
+    var output = await FlutterImageCompress.compressWithList(input,
+      format:CompressFormat.webp,
+      quality: 100,
+      minWidth: 10,
+      minHeight: 10,
+    );
+    return output;
   }
 
   @override
