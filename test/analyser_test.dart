c66914e14944b78620c2d7499cfbb2ae0e5957ef
n7484443 <n7484443@naver.com>
2022-05-05 21:28:56 +0900

    -테스트 코드 정리

--- test/analyser_test.dart
+++ test/analyser_test.dart
@@ -4,63 +4,18 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   var ins = VariableDataBase();
-  String strTest = """
-                var numberTest0 = -5.5
-                var numberTest1 = 3
-                var numberTest2 = 3 + 5
-                var numberTest3 = 6 - 5.5
-                var numberTest4 = 6 * 3
-                var numberTest5 = 6 / 3
-                
-                var boolTest1 = true
-                var boolTest2 = false
-                
-                var roundTest = round(4.8)
-                var ceilTest = ceil(4.8)
-                var floorTest = floor(4.8)
-                
-                var stringTest = "문자열 테스트String1"
-                var stringAddTest = "문자" + "열테스트1"
-                var stringAddTest1 = "문자열테스트1"
-                var stringAddTest2 = stringAddTest == stringAddTest1
-                
-                var comp1 = numberTest0 == -5.5
-                var comp2 = numberTest0 >= -5.5
-                var comp3 = numberTest0 > -5.5
-                if(numberTest0 == -5.5, var alpha = 11, var beta = 15)
-                if(numberTest0 != -5.5, var gamma = 12, var omega = 16)
-                var test_alpha = 1
-                test_alpha+=3
-                var test_beta = 1
-                test_beta -= 5
-                
-                var and_test_alpha = true
-                var and_test_beta = true
-                var and_test_1 = and(true, and_test_beta, true, true, true, true)
-                var and_test_2 = and(false, true, true, true)
-                var and_test_3 = and(true, false)
-                var and_test_4 = and(false, false)
-                
-                var multiple_test_1 = and(not(and(true, false)), true)
-                
-                let globalTest = 123
-                """;
-  String strTest2 = """
-                numberTest0 < 0
-  """;
-  String strTest3 = """ 
-                numberTest4 >= 19
-  """;
-  String strGlobalTest = """
-                if(globalTest == 123, var T = true, var T = false)
-                var existTest = exist(T)
-                var existTest2 = exist(numberTest0)
-                """;
-  var recursiveData = Analyser().analyseCodes(strTest);
-  Analyser().run(recursiveData);
-
   var epsilon = 0.00001;
-  test('numberTest', (){
+
+  test('numberTest', () {
+    String strTest = """
+    let numberTest0 = -5.5
+    let numberTest1 = 3
+    let numberTest2 = 3 + 5
+    let numberTest3 = 6 - 5.5
+    let numberTest4 = 6 * 3
+    let numberTest5 = 6 / 3
+    """;
+    Analyser().run(Analyser().analyseCodes(strTest));
     expect(ins.getValueType('numberTest0')?.data, closeTo(-5.5, epsilon));
     expect(ins.getValueType('numberTest1')?.data, 3);
     expect(ins.getValueType('numberTest2')?.data, 8);
@@ -68,66 +23,128 @@ void main() {
     expect(ins.getValueType('numberTest4')?.data, 18);
     expect(ins.getValueType('numberTest5')?.data, 2);
   });
-
-  test('boolTest', (){
+  test('boolTest', () {
+    String strTest = """                
+    var boolTest1 = true
+    var boolTest2 = false
+    """;
+    Analyser().run(Analyser().analyseCodes(strTest));
     expect(ins.getValueType('boolTest1')?.data, true);
     expect(ins.getValueType('boolTest2')?.data, false);
   });
 
-  test('funcTest', (){
+  test('funcTest', () {
+    String strTest = """
+    var roundTest = round(4.8)
+    var ceilTest = ceil(4.8)
+    var floorTest = floor(4.8)
+    """;
+    Analyser().run(Analyser().analyseCodes(strTest));
     expect(ins.getValueType('roundTest')?.data, 5);
     expect(ins.getValueType('ceilTest')?.data, 5);
     expect(ins.getValueType('floorTest')?.data, 4);
   });
 
-  test('stringTest', (){
+  test('stringTest', () {
+    String strTest = """
+    var stringTest = "문자열 테스트String1"
+    var stringAddTest = "문자" + "열테스트1"
+    var stringAddTest1 = "문자열테스트1"
+    var stringAddTest2 = stringAddTest == stringAddTest1
+    """;
+    Analyser().run(Analyser().analyseCodes(strTest));
     expect(ins.getValueType('stringTest')?.data, "문자열 테스트String1");
     expect(ins.getValueType('stringAddTest')?.data, "문자열테스트1");
     expect(ins.getValueType('stringAddTest1')?.data, "문자열테스트1");
     expect(ins.getValueType('stringAddTest2')?.data, true);
   });
 
-  test('compTest', (){
+  test('compTest', () {
+    String strTest = """
+    var comp1 = numberTest0 == -5.5
+    var comp2 = numberTest0 >= -5.5
+    var comp3 = numberTest0 > -5.5
+    """;
+    Analyser().run(Analyser().analyseCodes(strTest));
     expect(ins.getValueType('comp1')?.data, true);
     expect(ins.getValueType('comp2')?.data, true);
     expect(ins.getValueType('comp3')?.data, false);
   });
 
-  test('ifTest', (){
+  test('ifTest', () {
+    String strTest = """
+    if(numberTest0 == -5.5, var alpha = 11, var beta = 15)
+    if(numberTest0 != -5.5, var gamma = 12, var omega = 16)
+    """;
+    Analyser().run(Analyser().analyseCodes(strTest));
     expect(ins.getValueType('alpha')?.data, 11);
     expect(ins.getValueType('beta')?.data, null);
     expect(ins.getValueType('gamma')?.data, null);
     expect(ins.getValueType('omega')?.data, 16);
   });
-  test('addTest', (){
+  test('addTest', () {
+    var addTestStr = """
+    var test_alpha = 1
+    test_alpha+=3
+    var test_beta = 1
+    test_beta -= 5
+    """;
+    Analyser().run(Analyser().analyseCodes(addTestStr));
     expect(ins.getValueType('test_alpha')?.data, 4);
     expect(ins.getValueType('test_beta')?.data, -4);
   });
 
-  test('and Test', (){
+  test('and Test', () {
+    var andTestStr = """
+    var and_test_alpha = true
+    var and_test_beta = true
+    var and_test_1 = and(true, and_test_beta, true, true, true, true)
+    var and_test_2 = and(false, true, true, true)
+    var and_test_3 = and(true, false)
+    var and_test_4 = and(false, false)
+    """;
+    Analyser().run(Analyser().analyseCodes(andTestStr));
+
     expect(ins.getValueType('and_test_1')?.data, true);
     expect(ins.getValueType('and_test_2')?.data, false);
     expect(ins.getValueType('and_test_3')?.data, false);
     expect(ins.getValueType('and_test_4')?.data, false);
   });
 
-
-  test('multiple Test', (){
+  test('multiple Test', () {
+    String strTest = """
+    var multiple_test_1 = and(not(and(true, false)), true)
+    """;
+    Analyser().run(Analyser().analyseCodes(strTest));
     expect(ins.getValueType('multiple_test_1')?.data, true);
   });
 
-  test('return Test', (){
-    var recursiveData = Analyser().analyseCodes(strTest2);
+  test('return Test', () {
+    String strTest = """
+    numberTest0 < 0
+    """;
+    String strTest1 = """ 
+    numberTest4 >= 19
+    """;
+    var recursiveData = Analyser().analyseCodes(strTest);
     expect(Analyser().check(recursiveData[0]) as bool, true);
 
-    recursiveData = Analyser().analyseCodes(strTest3);
+    recursiveData = Analyser().analyseCodes(strTest1);
     expect(Analyser().check(recursiveData[0]) as bool, false);
   });
 
-  test('global variable Test', (){
-    recursiveData = Analyser().analyseCodes(strGlobalTest);
-    VariableDataBase().clearLocalVariable();
-    Analyser().run(recursiveData);
+  test('global variable Test', () {
+    String strTest = """
+    let globalTest = 123
+    """;
+    String strGlobalTest = """
+    if(globalTest == 123, var T = true, var T2 = false)
+    var existTest = exist(T)
+    var existTest2 = exist(T2)
+    """;
+    Analyser().run(Analyser().analyseCodes(strTest));
+    ins.clearLocalVariable();
+    Analyser().run(Analyser().analyseCodes(strGlobalTest));
     expect(ins.getValueType('T')?.data, true);
     expect(ins.getValueType('existTest')?.data, true);
     expect(ins.getValueType('existTest2')?.data, false);
