bc403aeb2731e2920a7dd2d891b52dfcfbd95765
n7484443 <n7484443@naver.com>
2022-08-18 09:17:18 +0900

    Feat: 이미지 설정의 카드 디자인 변경

--- lib/view/view_source.dart
+++ lib/view/view_source.dart
@@ -2,7 +2,6 @@ import 'package:cyoap_flutter/view/util/view_image_loading.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../main.dart';
 import '../viewModel/vm_make_platform.dart';
 import '../viewModel/vm_source.dart';
 
@@ -16,49 +15,55 @@ class ViewSource extends ConsumerWidget {
     var widget = ListView.builder(
       itemBuilder: (BuildContext context, int index) {
         var name = controller[index];
-        return Card(
-          elevation: ConstList.elevation,
-          color: deleteList.contains(name) ? Colors.redAccent : Colors.white,
-          child: Row(
-            children: [
-              Expanded(
-                flex: 1,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Card(
+            elevation: 0,
+            shape: RoundedRectangleBorder(
+              side: BorderSide(
+                color: Theme.of(context).colorScheme.outline,
+              ),
+              borderRadius: const BorderRadius.all(Radius.circular(12)),
+            ),
+            color: deleteList.contains(name) ? Theme.of(context).colorScheme.secondaryContainer : null,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
                   child: ViewImageLoading(name),
                 ),
-              ),
-              Flexible(
-                flex: 5,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text(name),
-                      TextField(
-                        textAlign: TextAlign.start,
-                        decoration: const InputDecoration(
-                          hintText: '출처를 여기에 적어주세요!',
-                          alignLabelWithHint: true,
+                Flexible(
+                  flex: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(name),
+                        TextField(
+                          textAlign: TextAlign.start,
+                          decoration: const InputDecoration(
+                            hintText: '출처를 여기에 적어주세요!',
+                            alignLabelWithHint: true,
+                          ),
+                          controller:
+                              ref.watch(textEditingControllerProvider(name)),
                         ),
-                        controller:
-                            ref.watch(textEditingControllerProvider(name)),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Visibility(
-                visible: ref.watch(deleteModeProvider),
-                child: IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () {
-                    ref.read(vmSourceProvider.notifier).checkRemove(name);
-                  },
+                Visibility(
+                  visible: ref.watch(deleteModeProvider),
+                  child: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () {
+                      ref.read(vmSourceProvider.notifier).checkRemove(name);
+                    },
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         );
       },
