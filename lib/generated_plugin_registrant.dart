9f582b00bb0bb4b0a5c67db13e48ca04c9f59c96
n7484443 <n7484443@naver.com>
2022-09-08 07:26:12 +0900

    Fix: 숨겨진 줄이 공간을 차지하는 문제 수정 Perf

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -9,12 +9,10 @@
 import 'package:device_info_plus_web/device_info_plus_web.dart';
 import 'package:file_picker/_internal/file_picker_web.dart';
 import 'package:flutter_keyboard_visibility_web/flutter_keyboard_visibility_web.dart';
-import 'package:image_picker_for_web/image_picker_for_web.dart';
 import 'package:package_info_plus_web/package_info_plus_web.dart';
 import 'package:sentry_flutter/sentry_flutter_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
-import 'package:video_player_web/video_player_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -23,11 +21,9 @@ void registerPlugins(Registrar registrar) {
   DeviceInfoPlusPlugin.registerWith(registrar);
   FilePickerWeb.registerWith(registrar);
   FlutterKeyboardVisibilityPlugin.registerWith(registrar);
-  ImagePickerPlugin.registerWith(registrar);
   PackageInfoPlugin.registerWith(registrar);
   SentryFlutterWeb.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
-  VideoPlayerPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
