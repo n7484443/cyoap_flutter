52e0de1cc73a337f33bb8be703690566eb62fb7d
n7484443 <n7484443@naver.com>
2022-10-02 05:02:03 +0900

    Feat: 이미지 설정 탭도 여러개 한번에 보이도록 수정

--- lib/view/view_source.dart
+++ lib/view/view_source.dart
@@ -1,77 +1,40 @@
+import 'package:cyoap_flutter/main.dart';
 import 'package:cyoap_flutter/view/util/view_image_loading.dart';
+import 'package:cyoap_flutter/view/view_draggable_nested_map.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../viewModel/vm_make_platform.dart';
 import '../viewModel/vm_source.dart';
 
-class ViewSource extends ConsumerWidget {
-  const ViewSource({super.key});
+class ViewSource extends ConsumerStatefulWidget {
+  const ViewSource({
+    super.key,
+  });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState createState() => _ViewSourceState();
+}
+
+class _ViewSourceState extends ConsumerState<ViewSource> {
+  ScrollController? _controller;
+
+  @override
+  void initState() {
+    _controller = AdjustableScrollController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     var data = ref.watch(vmSourceProvider);
     var deleteList = ref.watch(deleteImageListProvider);
-    var widget = ListView.builder(
-      controller: ScrollController(),
-      itemBuilder: (BuildContext context, int index) {
-        var name = data[index];
-        return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Card(
-            elevation: 0,
-            shape: RoundedRectangleBorder(
-              side: BorderSide(
-                color: Theme.of(context).colorScheme.outline,
-              ),
-              borderRadius: const BorderRadius.all(Radius.circular(12)),
-            ),
-            color: deleteList.contains(name)
-                ? Theme.of(context).colorScheme.secondaryContainer
-                : null,
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: ViewImageLoading(name),
-                ),
-                Flexible(
-                  flex: 5,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(name),
-                        TextField(
-                          textAlign: TextAlign.start,
-                          decoration: const InputDecoration(
-                            hintText: '출처를 여기에 적어주세요!',
-                            alignLabelWithHint: true,
-                          ),
-                          controller:
-                              ref.watch(textEditingControllerProvider(name)),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Visibility(
-                  visible: ref.watch(deleteModeProvider),
-                  child: IconButton(
-                    icon: const Icon(Icons.delete),
-                    onPressed: () {
-                      ref.read(vmSourceProvider.notifier).checkRemove(name);
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-      itemCount: data.length,
-    );
 
     Widget icon;
     if (!ref.watch(deleteModeProvider)) {
@@ -122,7 +85,72 @@ class ViewSource extends ConsumerWidget {
           ),
           title: icon,
         ),
-        body: widget,
+        body: CustomScrollView(
+          slivers: [
+            SliverGrid(
+                delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    var name = data[index];
+                    return Card(
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                          color: Theme.of(context).colorScheme.outline,
+                        ),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(12)),
+                      ),
+                      color: deleteList.contains(name)
+                          ? Theme.of(context).colorScheme.secondaryContainer
+                          : null,
+                      child: Column(
+                        children: [
+                          Stack(children: [
+                            Align(
+                              alignment: Alignment.center,
+                              child: Text(name),
+                            ),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: Visibility(
+                                visible: ref.watch(deleteModeProvider),
+                                child: IconButton(
+                                  icon: const Icon(Icons.delete),
+                                  onPressed: () {
+                                    ref
+                                        .read(vmSourceProvider.notifier)
+                                        .checkRemove(name);
+                                  },
+                                ),
+                              ),
+                            ),
+                          ]),
+                          Expanded(
+                            child: ViewImageLoading(name),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextField(
+                              textAlign: TextAlign.start,
+                              decoration: const InputDecoration(
+                                hintText: '출처를 여기에 적어주세요!',
+                                alignLabelWithHint: true,
+                              ),
+                              controller: ref
+                                  .watch(textEditingControllerProvider(name)),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  childCount: data.length,
+                ),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: ConstList.isSmallDisplay(context) ? 2 : 3)),
+          ],
+          controller: _controller,
+        ),
       ),
       onWillPop: () async {
         return false;
