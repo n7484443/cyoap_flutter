159a2f0f1723e8c3a15db5f0c5351af9a70517bb
n7484443 <n7484443@naver.com>
2022-11-01 22:29:39 +0900

    Initial windows size and position modify

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -25,8 +25,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
+  Win32Window::Point origin(20, 20);
+  Win32Window::Size size(1600, 900);
   if (!window.CreateAndShow(L"cyoap_flutter", origin, size)) {
     return EXIT_FAILURE;
   }
