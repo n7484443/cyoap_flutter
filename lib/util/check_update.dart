97c35aff92826c439f9070cde95f5ca05e851d64
n7484443 <n7484443@naver.com>
2022-11-02 19:21:43 +0900

    레포지트리 에러 처리 추가

--- lib/util/check_update.dart
+++ lib/util/check_update.dart
@@ -6,20 +6,24 @@ import 'version.dart';
 
 class CheckUpdate {
   static Future<bool> needUpdateCheck() async {
-    var github = GitHub();
+    try{
+      var github = GitHub();
 
-    var lastRelease = await github.repositories
-        .listReleases(RepositorySlug('n7484443', 'FlutterCyoap'))
-        .first;
-    if (kDebugMode) {
-      print(
-          '마지막 업데이트 버전 : ${lastRelease.tagName} | 현제 버전 : v${ConstList.version}');
+      var lastRelease = await github.repositories
+          .listReleases(RepositorySlug('n7484443', 'FlutterCyoap'))
+          .first;
+      if (kDebugMode) {
+        print(
+            '마지막 업데이트 버전 : ${lastRelease.tagName} | 현제 버전 : v${ConstList.version}');
+      }
+      if (lastRelease.tagName == null) return false;
+      if (ConstList.version == '') return false;
+      if (versionCheck(lastRelease.tagName!, ConstList.version) > 0) {
+        return true;
+      }
+      return false;
+    }catch(e){
+      return false;
     }
-    if (lastRelease.tagName == null) return false;
-    if (ConstList.version == '') return false;
-    if (versionCheck(lastRelease.tagName!, ConstList.version) > 0) {
-      return true;
-    }
-    return false;
   }
 }
