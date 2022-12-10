50557c6ba89431682f685655313220a615ed6ee2
n7484443 <n7484443@naver.com>
2022-12-11 06:49:02 +0900

    dependency upgrade

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -7,10 +7,10 @@
 // ignore_for_file: depend_on_referenced_packages
 
 import 'package:desktop_drop/desktop_drop_web.dart';
-import 'package:device_info_plus_web/device_info_plus_web.dart';
+import 'package:device_info_plus/src/device_info_plus_web.dart';
 import 'package:file_picker/_internal/file_picker_web.dart';
 import 'package:flutter_keyboard_visibility_web/flutter_keyboard_visibility_web.dart';
-import 'package:package_info_plus_web/package_info_plus_web.dart';
+import 'package:package_info_plus/src/package_info_plus_web.dart';
 import 'package:sentry_flutter/sentry_flutter_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
@@ -20,10 +20,10 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
   DesktopDropWeb.registerWith(registrar);
-  DeviceInfoPlusPlugin.registerWith(registrar);
+  DeviceInfoPlusWebPlugin.registerWith(registrar);
   FilePickerWeb.registerWith(registrar);
   FlutterKeyboardVisibilityPlugin.registerWith(registrar);
-  PackageInfoPlugin.registerWith(registrar);
+  PackageInfoPlusWebPlugin.registerWith(registrar);
   SentryFlutterWeb.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
