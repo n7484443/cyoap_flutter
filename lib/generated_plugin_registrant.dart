7532b5ced76dc7c271a3d8bfb4dc2e2480cf2a6c
n7484443 <n7484443@naver.com>
2022-12-29 22:45:44 +0900

    Formatting

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -10,12 +10,11 @@ import 'package:desktop_drop/desktop_drop_web.dart';
 import 'package:device_info_plus/src/device_info_plus_web.dart';
 import 'package:file_picker/_internal/file_picker_web.dart';
 import 'package:flutter_keyboard_visibility_web/flutter_keyboard_visibility_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:package_info_plus/src/package_info_plus_web.dart';
 import 'package:sentry_flutter/sentry_flutter_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
-
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
