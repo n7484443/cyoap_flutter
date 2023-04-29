4f023b95e9bffd27b5fa79db4a09c778219ca11b
n7484443 <n7484443@users.noreply.github.com>
2023-04-29 22:20:53 +0000

    deploy: daf8208ee629eda1675f454908e82ce72bbb416b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,10 +23,10 @@ const RESOURCES = {
 "index.html": "58f905882ef645859ad24d2c84419dde",
 "/": "58f905882ef645859ad24d2c84419dde",
 "jszip.js": "418ea325c68eb099ef2ccc8823771a63",
-"main.dart.js": "29e3cf60bb3828d8df0a393916262b3c",
+"main.dart.js": "bece93d91eb8db719562469ff4736927",
 "manifest.json": "ebc04e4594e86d4e5763ccf0df7285b3",
 "save_web.js": "6d7f19b0443d2ca2a960f0532299e5d7",
-"version.json": "983a66b9eed3871ca4078242021d884a",
+"version.json": "65369f201222ff148fceeb3346a383ba",
 "worker.js": "55118d4a43a00cedd42f65ca3b0b0d2a"
 };
 
