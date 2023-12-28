4147d92a029c4a8b35d3dfd8612e3333990fb059
n7484443 <n7484443@users.noreply.github.com>
2023-12-28 11:55:21 +0000

    deploy: 69ad0da3d654a14961f848afdfe84daa2c8dd41c

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,11 +7,11 @@ const RESOURCES = {"assets/AssetManifest.bin": "5f7db09af6875d1b5a7921b045495873
 "assets/AssetManifest.bin.json": "55f1faacabb98402c13a6728896ad50d",
 "assets/AssetManifest.json": "0bd258f452c5f44ab34dc53b945ef114",
 "assets/FontManifest.json": "df625e53b2aec5cd1d311615484480d0",
-"assets/fonts/MaterialIcons-Regular.otf": "7006d85de396524babfe42e105b5e4f1",
+"assets/fonts/MaterialIcons-Regular.otf": "859057e2130bf298e97085bf6fec5819",
 "assets/fonts/NeoDGM/NeoDGM.ttf": "fb933425ff4ed74ed3b810b399b7946f",
 "assets/images/checkers.png": "832cfa26861372530aef1964499c94ff",
 "assets/images/sample.png": "c92e1697d57a0d1e7007fe33d76ea125",
-"assets/NOTICES": "34e37abcb846cf660479c45eabbdd025",
+"assets/NOTICES": "44bd0302d184935923206d5633fbeaf7",
 "assets/packages/flex_color_picker/assets/opacity.png": "49c4f3bcb1b25364bb4c255edcaaf5b2",
 "assets/packages/flutter_image_compress_web/assets/pica.min.js": "6208ed6419908c4b04382adc8a3053a2",
 "assets/packages/window_manager/images/ic_chrome_close.png": "75f4b8ab3608a05461a31fc18d6b47c2",
@@ -35,10 +35,10 @@ const RESOURCES = {"assets/AssetManifest.bin": "5f7db09af6875d1b5a7921b045495873
 "index.html": "58f905882ef645859ad24d2c84419dde",
 "/": "58f905882ef645859ad24d2c84419dde",
 "jszip.js": "418ea325c68eb099ef2ccc8823771a63",
-"main.dart.js": "473a2c0dfdaefffdd36866af7bfa730a",
+"main.dart.js": "1927f3a2454074ea811919eb117465d0",
 "manifest.json": "ebc04e4594e86d4e5763ccf0df7285b3",
 "save_web.js": "6d7f19b0443d2ca2a960f0532299e5d7",
-"version.json": "24c1f65bb54c49c546f0169c70bb90fd",
+"version.json": "d17d774daba46b85b8b123b25b3d9398",
 "worker.js": "55118d4a43a00cedd42f65ca3b0b0d2a"};
 // The application shell files that are downloaded before a service worker can
 // start.
