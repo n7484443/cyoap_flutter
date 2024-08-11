362e4f20bba76364861655a379d8d9d86de2d915
n7484443 <n7484443@naver.com>
2024-08-11 17:24:12 +0900

    fix

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -6,7 +6,6 @@
 // ignore_for_file: lines_longer_than_80_chars
 // ignore_for_file: depend_on_referenced_packages
 
-import 'package:desktop_drop/desktop_drop_web.dart';
 import 'package:device_info_plus/src/device_info_plus_web.dart';
 import 'package:file_picker/_internal/file_picker_web.dart';
 import 'package:flutter_keyboard_visibility_web/flutter_keyboard_visibility_web.dart';
@@ -18,7 +17,6 @@ import 'package:url_launcher_web/url_launcher_web.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
-  DesktopDropWeb.registerWith(registrar);
   DeviceInfoPlusWebPlugin.registerWith(registrar);
   FilePickerWeb.registerWith(registrar);
   FlutterKeyboardVisibilityPlugin.registerWith(registrar);
