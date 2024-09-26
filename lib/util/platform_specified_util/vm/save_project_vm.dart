db672870fff729815fc7cdfe5320ef44dd003a4c
n7484443 <n7484443@naver.com>
2024-09-26 22:38:46 +0900

    add feature: file duplication prevent

--- lib/util/platform_specified_util/vm/save_project_vm.dart
+++ lib/util/platform_specified_util/vm/save_project_vm.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:archive/archive.dart';
+import 'package:crypto/crypto.dart';
 import 'package:cyoap_flutter/main.dart';
 import 'package:cyoap_flutter/util/platform_specified_util/platform_specified.dart';
 import 'package:file_picker/file_picker.dart';
@@ -44,9 +45,33 @@ class SaveProjectImp extends SaveProject {
     await file.writeAsBytes(uint8data);
   }
 
+  Future<bool> checkZipSameFile(Uint8List a, Uint8List b) async {
+    var zip = ZipDecoder();
+    var archiveA = zip.decodeBytes(a);
+    var archiveB = zip.decodeBytes(b);
+    if (archiveA.length != archiveB.length) return false;
+    for (int i = 0; i < archiveA.length; i++) {
+      if (archiveA[i].name != archiveB[i].name) return false;
+      var hashA =
+          await calculateFileHash(data: archiveA[i].rawContent!.toUint8List());
+      var hashB =
+          await calculateFileHash(data: archiveB[i].rawContent!.toUint8List());
+      if (hashA != hashB) return false;
+    }
+    return true;
+  }
+
   @override
   Future<void> saveBackup(String path, Map<String, Uint8List> dataInput) async {
     var uint8data = await compute(mapToArchive, dataInput);
+
+    var latest = await findMostRecentlyModifiedFile('$path/backup');
+    if (latest != null &&
+        await checkZipSameFile(uint8data, await latest.readAsBytes())) {
+      print("same backup file. skip this backup");
+      return;
+    }
+
     var currentTime = DateTime.now();
     var formatDate =
         "${currentTime.year}-${currentTime.month}-${currentTime.day}_${currentTime.hour}-${currentTime.minute}";
@@ -56,6 +81,43 @@ class SaveProjectImp extends SaveProject {
     }
     var file = File(name);
     await file.writeAsBytes(uint8data);
+  }
+
+  Future<File?> findMostRecentlyModifiedFile(String directoryPath) async {
+    final directory = Directory(directoryPath);
+
+    if (!await directory.exists()) {
+      print('Directory does not exist: $directoryPath');
+      return null;
+    }
+
+    File? mostRecentFile;
+    DateTime mostRecentModification = DateTime.fromMillisecondsSinceEpoch(0);
+
+    await for (final entity
+        in directory.list(recursive: false, followLinks: false)) {
+      if (entity is File) {
+        final lastModified = await entity.lastModified();
+        if (lastModified.isAfter(mostRecentModification)) {
+          mostRecentModification = lastModified;
+          mostRecentFile = entity;
+        }
+      }
+    }
+
+    return mostRecentFile;
+  }
+
+  Future<String> calculateFileHash({File? file, Uint8List? data}) async {
+    assert(file != null || data != null);
+    try {
+      final fileContents = data ?? await file!.readAsBytes();
+      final hash = sha256.convert(fileContents);
+      return hash.toString();
+    } catch (e) {
+      print('Error calculating hash: $e');
+      return '';
+    }
   }
 
   @override
