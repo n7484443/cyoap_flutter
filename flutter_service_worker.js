2372251e289be0547c3ce8724367041ffdc76b4a
n7484443 <n7484443@users.noreply.github.com>
2025-04-06 02:09:34 +0000

    deploy: b3d766688c9006a2eaf0289e77add1947f95b09d

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3f115035bb46d80810b0c57f27923ab4
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "7434dd119f387eb7f815653d2ba4bca6",
+"flutter_bootstrap.js": "ee3ef4c48115d9b13d87ebb554c09603",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
